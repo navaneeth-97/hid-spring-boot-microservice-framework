3cd1416c1e5648398ddb3dcd9299b670735ca856
N, Navaneeth <navann@deloitte.com>
2023-01-25 11:12:09 +0530

    "Removed need of separate template for maven and gradle "

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,7 @@ from cookiecutter.main import cookiecutter
 
 import os
 import shutil
-
+import glob
 
 TERMINATOR = "\x1b[0m"
 INFO = "\x1b[1;33m [INFO]: "
@@ -15,7 +15,7 @@ def main():
     project_name = "{{ cookiecutter.project_name }}"
 
     templates_repo = "{{ cookiecutter._templates_repo }}"
-    template_dir = os.path.join("templates", "{{ cookiecutter.java_version }}", "{{ cookiecutter.dependency_manager.lower() }}", "{{ cookiecutter.authentication }}")
+    template_dir = os.path.join("templates", "{{ cookiecutter.java_version }}", "{{ cookiecutter.authentication }}")
     
     cookiecutter(   
                     templates_repo,
@@ -27,6 +27,30 @@ def main():
                                     "project_name": project_name
                                   }
                 )
+
+    if '{{ cookiecutter.dependency_manager }}' == 'Maven':
+        all_delete_files = glob.glob(os.path.join('.', '*gradle*'), recursive=True)
+        for f in all_delete_files:
+            shutil.rmtree(f) if os.path.isdir(f) else os.remove(f)
+#         open('.versioning', 'a+').write(
+#             '{}\
+#             \nfilepath = "pom.xml"\
+#             \npattern = "<version>{}</version>"\
+#             '.format(ver_syntax, '${version}')
+#         )
+    else:
+        os.remove('pom.xml')
+        os.remove('mvnw')
+        os.remove('mvnw.cmd')
+#         open('.versioning', 'a+').write(
+#             '{}\
+#             \nfilepath = "gradle.properties"\
+#             \npattern = "version={}"\
+#             \n{}\
+#             \nfilepath = "build.gradle"\
+#             \npattern = "version = {}"\
+#             '.format(ver_syntax, '${version}', ver_syntax, '${version}')
+#         )
 
     print(SUCCESS +
           "Project initialized successfully! You can now jump to {} folder".
